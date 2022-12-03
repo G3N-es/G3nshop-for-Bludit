2e70df6560842d55b5b2f3edd7c6cee80a8c0960
G3N-es <45354881+G3N-es@users.noreply.github.com>
2022-12-03 12:58:13 +0100

    Delete example_content/bl-content/databases/plugins directory

--- example_content/bl-content/databases/plugins/about/db.php
+++
@@ -1,2 +0,0 @@
-<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{"position":"2","label":"Acerca de","text":"Breve descripci\u00f3n de ti o de tu sitio, para cambiar este texto, vaya al panel de administraci\u00f3n, ajustes, complementos, y configurar el complemento Acerca de"}
