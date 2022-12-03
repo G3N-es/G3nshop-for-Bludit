15fbd4eeb56cc40e4162106daa9280df17822edf
G3N-es <45354881+G3N-es@users.noreply.github.com>
2022-12-03 14:47:09 +0100

    Add files via upload

--- G3Nshop_theme/php/side-filters.php
+++ G3Nshop_theme/php/side-filters.php
@@ -22,13 +22,15 @@
 			</div>
 
 			<div id="caja-de-filtros">
+			<?php if(empty($filtrosEtiqueta) === false ){ ?>
 				<h4><?php echo $language->p('tags'); ?></h4>					
 				<ul class="tags genericas">
 				<?php foreach ($filtrosEtiqueta as $tagKey=>$tagName):?>
 					<li class="<?php echo $tagKey?>" ><a href="<?php echo DOMAIN_TAGS.$tagKey."?GS" ?>"><?php echo $tagName; ?></a></li>
 				<?php endforeach ?>
 				</ul>
-				
+			<?php } ?>
+			<?php if(empty($filtrosPrecio) === false ){ ?>	
 				<h4><?php echo $language->p('precios'); ?></h4>	
 				<ul class="tags precios">
 			<?php	ksort($filtrosPrecio);
@@ -36,20 +38,23 @@
 					<li class="<?php echo "precio ".$tagKey ?>" ><a href="<?php echo DOMAIN_TAGS.$tagKey."?GS" ?>"><?php echo $tagName." ".$moneda; ?></a></li>
 			<?php endforeach ?>
 				</ul>
-				
+			<?php } ?>
+			<?php if(empty($filtrosTalla) === false ){ ?>	
 				<h4><?php echo $language->p('tallas'); ?></h4>	
 				<ul class="tags tallas">
 				<?php foreach ($filtrosTalla as $tagKey=>$tagName):?>
 					<li class="<?php echo "talla ".$tagKey?>" ><a href="<?php echo DOMAIN_TAGS.$tagKey."?GS" ?>"><?php echo $tagName; ?></a></li>
 				<?php endforeach ?>
 				</ul>
-				
+			<?php } ?>
+			<?php if(empty($filtrosColor) === false ){ ?>	
 				<h4><?php echo $language->p('colores'); ?></h4>	
 				<ul class="tags colores">
 				<?php foreach ($filtrosColor as $tagKey=>$tagName):?>
 					<li class="<?php echo "color ".$tagKey?>" ><a href="<?php echo DOMAIN_TAGS.$tagKey."?GS" ?>"><?php echo $tagName; ?></a></li>
 				<?php endforeach ?>
 				</ul>
+			<?php } ?>
 			</div>
 		  </div>
 		</div>
